e63d127ddd1ffc975e7024e4b71c2d46b459dd8e
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-24 15:51:42 -0400

    setting up hidden api credentials and data fetch practice

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useEffect, useState } from "react";
+import { CLIENT_SECRET, CLIENT_ID } from "./config";
 
 function App() {
+  const [accessToken, setAccessToken] = useState("");
+
+  useEffect(() => {
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "POST",
+      body: new URLSearchParams({
+        grant_type: "client_credentials",
+        client_id: CLIENT_ID,
+        client_secret: CLIENT_SECRET,
+      }),
+    })
+      .then((r) => r.json())
+      .then((data) => setAccessToken(data.access_token));
+  }, []);
+
+  if (accessToken) {
+    fetch(
+      "https://api.spotify.com/v1/search?q=see+you+again&type=track&limit=6",
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      }
+    )
+      .then((r) => r.json())
+      .then((data) => console.log(data));
+  }
   return (
     <div className="App">
       <header className="App-header">
