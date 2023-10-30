fa09f1ded961b88470076d2a0e13215330c3bada
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-10-30 13:47:56 -0400

    controlled form to add song in home page

--- src/components/SongForm.js
+++ src/components/SongForm.js
@@ -1,5 +1,36 @@
-function SongForm() {
-  return <form></form>;
+function SongForm({ newSong, onUpdateNewSong }) {
+  return (
+    <form>
+      <input
+        type="text"
+        name="title"
+        placeholder="Song Title..."
+        value={newSong.title}
+        onChange={onUpdateNewSong}
+      />
+      <input
+        type="text"
+        name="album"
+        placeholder="Album Name..."
+        value={newSong.album}
+        onChange={onUpdateNewSong}
+      />
+      <input
+        type="text"
+        name="artist"
+        placeholder="Artist..."
+        value={newSong.artist}
+        onChange={onUpdateNewSong}
+      />
+      <input
+        type="text"
+        name="art"
+        placeholder="Album Art Link..."
+        value={newSong.art}
+        onChange={onUpdateNewSong}
+      />
+    </form>
+  );
 }
 
 export default SongForm;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,21 @@
+import { useState } from "react";
+import SongForm from "../components/SongForm";
+
 function Home() {
-  return <main>Home</main>;
+  const [newSong, setNewSong] = useState({
+    title: "",
+    album: "",
+    artist: "",
+    art: "",
+  });
+  function handleUpdateNewSong(event) {
+    setNewSong({ ...newSong, [event.target.name]: event.target.value });
+  }
+  return (
+    <main>
+      <SongForm newSong={newSong} onUpdateNewSong={handleUpdateNewSong} />
+    </main>
+  );
 }
 
 export default Home;
